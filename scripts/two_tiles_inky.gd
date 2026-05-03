895e515240e789833968a7a475b307c70feaee2c
GabrielXP9908 <gabriel9908@outlook.de>
2026-05-03 15:38:53 +0200

    Inky no move on 2nd Try fixed

--- scripts/two_tiles_inky.gd
+++ scripts/two_tiles_inky.gd
@@ -4,19 +4,20 @@ func _process(_delta: float) -> void:
 	var direction = GameManager.getpacmandirection()
 	var tmp_position = GameManager.getpacmanposition()
 	var blinky_pos = GameManager.getblinkyposition()
+	var tmp2_pos: Vector2
 	
 	if (direction == 1):
-		global_position.y = tmp_position[1] - 16
-		global_position.x = tmp_position[0]
+		tmp2_pos.y = tmp_position[1] - 16
+		tmp2_pos.x = tmp_position[0]
 	elif (direction == 2):
-		global_position.x = tmp_position[0] + 16
-		global_position.y = tmp_position[1]
+		tmp2_pos.x = tmp_position[0] + 16
+		tmp2_pos.y = tmp_position[1]
 	elif (direction == 3):
-		global_position.y = tmp_position[1] + 16
-		global_position.x = tmp_position[0]
+		tmp2_pos.y = tmp_position[1] + 16
+		tmp2_pos.x = tmp_position[0]
 	elif (direction == 4):
-		global_position.x = tmp_position[0] - 16
-		global_position.y = tmp_position[1]
+		tmp2_pos.x = tmp_position[0] - 16
+		tmp2_pos.y = tmp_position[1]
 	
-	global_position = global_position + (global_position - blinky_pos)
+	global_position = tmp2_pos + (tmp2_pos - blinky_pos)
 	print(global_position)
